95633f44c6be997db18282f03e11cda35e2cdf4d
yuhuachang <ihuachangyu@gmail.com>
2022-10-17 21:10:32 +0800

    add data

--- src/main/java/task/common/CommonConfig.java
+++ src/main/java/task/common/CommonConfig.java
@@ -2,6 +2,8 @@ package task.common;
 
 public class CommonConfig {
     public static final boolean isLocal = true;
-    public static final String anomalyCsvPath = isLocal ? "data/anomaly_detection.csv" : "/share/anomaly_detection.csv";
-    public static final String smokeCsvPath = isLocal ? "data/smoke/dataset.csv" : "/share/smoke/dataset.csv";
+    public static final String anomalyCsvPath = isLocal ? "data/anomaly/anomaly_detection.csv"
+            : "/share/anomaly/anomaly_detection.csv";
+    public static final String smokeCsvPath = isLocal ? "data/smoke/dataset.csv"
+            : "/share/smoke/dataset.csv";
 }
